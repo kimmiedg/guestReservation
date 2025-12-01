be34bc35d578b1d08361a337b36efde2d1e7fd78
kimmie <kimberly.degracia@madsoftware.tech>
2025-12-01 16:22:38 +0800

    Added test for models

--- spec/models/guest.rb
+++ spec/models/guest.rb
@@ -4,32 +4,17 @@ RSpec.describe Guest, type: :model do
   describe "validations" do
     let(:guest1) { create(:guest) }
 
-    context "when email is valid and unique" do
+    context "when validating uniqueness" do
       it "should be valid" do
         expect(guest1).to be_valid
       end
-    end
 
-    context "when email exists" do
-      let(:guest2) { build(:guest, email: guest1.email) }
+      context "when email exists" do
+        let(:guest2) { create(:guest, email: guest1.email) }
 
-      it "should not be valid" do
-        expect(guest2).not_to be_valid
-        expect(guest2.errors[:email]).to include("has already been taken")
-      end
-    end
-
-    context "when phone is valid and unique" do
-      it "should be valid" do
-        expect(guest1).to be_valid
-      end
-    end
-
-    context "when phone exists" do
-      let(:guest2) { build(:guest, phone: guest1.phone) }
-
-      it "should not be valid" do
-        expect(guest2).not_to be_valid
+        it "should not be valid" do
+          expect(guest2).not_to be_valid
+        end
       end
     end
   end
--- spec/models/reservation.rb
+++ spec/models/reservation.rb
@@ -2,20 +2,16 @@ require "rails_helper"
 
 RSpec.describe Reservation, type: :model do
   describe "validations" do
-    context "when the attributes are valid" do
-      let!(:reservation1) { create(:reservation) }
+    let!(:reservation1) { create(:reservation) }
 
-      it "should be valid" do
-        expect(reservation1).to be_valid
-      end
+    context "when the attributes are valid" do
+      expect(reservation1).to be_valid
     end
 
     context "when the attributes are invalid" do
       let(:reservation2) { build(:reservation, start_date: "hello") }
 
-      it "should not be valid" do
-        expect(reservation2).not_to be_valid
-      end
+      expect(reservation2).not_to be_valid
     end
   end
 end
