63c25087cd456c1d52deead58a9035016a767ca6
kimmie <kimberly.degracia@madsoftware.tech>
2025-12-01 16:22:15 +0800

    Added models for Guest and Reservation

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,7 +3,4 @@
 class Guest < ApplicationRecord
   has_many :reservation_guests, dependent: :destroy
   has_many :reservations, through: :reservation_guests
-
-  validates :email, presence: true, uniqueness: true
-  validates :phone, presence: true, uniqueness: true
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,12 +4,16 @@ class Reservation < ApplicationRecord
   has_many :reservation_guests
   has_many :guests, through: :reservation_guests
 
+  #validations
   validate :must_be_valid_date
 
   def must_be_valid_date
-    return if start_date.is_a?(Date) && end_date.is_a?(Date)
-    errors.add("Must be valid date")
+    unless start_date.is_a?(Date) && end_date.is_a?(Date)
+      errors.add(:start_date, "must be a valid date")
+      errors.add(:end_date, "must be a valid date")
+    end
   rescue ArgumentError
-    errors.add("Must be valid date")
+    errors.add(:start_date, "must be a valid date")
+    errors.add(:end_date, "must be a valid date")
   end
 end
